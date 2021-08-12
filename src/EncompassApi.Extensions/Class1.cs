8e67cddaf39d349d79f530717960088ccc897fc0
Michael Seifi <michael.seifi@fairwaymc.com>
2021-08-12 16:01:15 -0500

    add main project targetting netcore 3.1 and netstandard 2.1

---
+++ src/EncompassApi.Extensions/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EncompassApi.Extensions
+{
+    public class Class1
+    {
+        public void test()
+        {
+#if NETCOREAPP3_1
+
+#elif NETSTANDARD2_0_OR_GREATER
+
+#endif
+        }
+    }
+}
